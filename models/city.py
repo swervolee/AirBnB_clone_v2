578a3eaddf2d5b8798a0ad37af782e37f57e647d
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-11-22 16:47:12 +0300

    a fix

--- models/city.py
+++ models/city.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This is the city class"""
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    __tablename__ = "cities"
+class City(BaseModel, Base):
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states_id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan', backref="cities")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state = relationship("State", backref="cities", foreign_keys=[state_id])
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,7 @@
 
 from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.state import State
@@ -14,12 +14,12 @@ from models.review import Review
 from models.amenity import Amenity
 
 class DBStorage():
-    """A sql database storage engine"""
+    """A SQL database storage engine"""
 
     __engine = None
     __session = None
 
-    def __innit__(self):
+    def __init__(self):
         user = getenv("HBNB_MYSQL_USER")
         passwd = getenv("HBNB_MYSQL_PWD")
         env = getenv("HBNB_ENV")
@@ -28,6 +28,49 @@ class DBStorage():
 
         self.__engine = create_engine(f"mysql+mysqldb://{user}:{passwd}@{host}/{db}", pool_pre_ping=True)
 
-
         if env == "test":
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """returns a dictionary of the query"""
+        dictionary = {}
+        if cls:
+            if type(cls) == str:
+                search = eval(cls)
+            query = self.__session.query(cls)
+
+            for item in query:
+                k = f"{type(item).__name__}.{item.id}"
+                dictionary[k] = item
+        else:
+            all = [State, City, User, Place, Review, Amenity]
+            for i in all:
+                query = self.__session.query(i)
+                for item in query:
+                    k = f"{type(item).__name__}.{item.id}"
+                    dictionary[k] = item
+        return dictionary
+
+    def new(self, obj):
+        """adds an object to the database"""
+        self.__session.add(obj)
+
+    def save(self):
+        """saves the pending changes"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """deletes an object from the database"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reads the database"""
+        Base.metadata.create_all(self.__engine)
+        sess = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        scoped_sess = scoped_session(sess)
+        self.__session = scoped_sess()
+
+    def close(self):
+        """Closes the session"""
+        self.__session.close()

a39cb7a22c1f12ac77a02d9ff696243f1ecc96f8
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-11-22 22:01:16 +0300

    update on place.py and user. py and city.py, adding attributes

--- models/place.py
+++ models/place.py
@@ -17,4 +17,8 @@ class Place(BaseModel, Base):
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
     number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullablle=True)
+    longitude = Column(Float, nullablle=True)

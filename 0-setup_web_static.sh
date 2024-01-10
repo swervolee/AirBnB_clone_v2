cd8f4f5d900f85785ae891853fa33819bfb1152e
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-10 22:42:52 +0300

    correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,6 +14,6 @@ echo "<!DOCTYPE html>
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R rhlee:rhlee /data/
+chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart

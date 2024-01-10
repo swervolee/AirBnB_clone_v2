2f15d434af1250c3cc1aab7e505d4e009ad98955
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-10 22:53:46 +0300

    correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,14 +5,13 @@ sudo apt-get -y update
 sudo apt install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "<!DOCTYPE html>
-<html>
+echo "<html>
   <head>
   </head>
   <body>
-    <p>Nginx server test</p>
+    Holberton School
   </body>
-</html>" | tee /data/web_static/releases/test/index.html
+</html>>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default

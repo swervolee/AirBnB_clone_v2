1feb3dbe06db5377911fe155e568dfb85b643d48
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-11 10:05:46 +0300

    correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,19 +2,19 @@
 """creates tar from web_static files and stores
 it in the folder versions
 """
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
 import os
 
 
 def do_pack():
+    """packs the webstatic files"""
     try:
         if not os.path.exists('versions'):
             local('mkdir versions')
-        local("tar -czvf versions/web_static_{}.tgz /web_static/".
+        local("tar -czvf versions/web_static_{}.tgz ./web_static/".
               format(datetime.now().strftime('%Y%m%d%H%M%S')))
-
-    except Exception:
+    except Exception as e:
         return None
     return ("web_static_{}.tgz".
             format(datetime.now().strftime('%Y%m%d%H%M%S')))

33f701e092157d577153001aeec2d4a2ecae1637
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-12 01:21:20 +0300

    a push

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 
 # Set the host IP addresses for web-01 && web-02
-env.hosts = ['18.234.105.167', '100.25.222.179']
+env.hosts = ['34.203.29.111', '3.84.158.149']
 env.user = "ubuntu"
 
 

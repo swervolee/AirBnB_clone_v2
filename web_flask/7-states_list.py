3af9ee49aa20fd4cab17a6a92e00e78106434856
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-28 15:28:13 +0300

    correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-
 Starts a Flask web application.
-
+The application listens on 0.0.0.0, port 5000.
+Routes:
+/states_list: HTML page with a list of all State objects in DBStorage.
 """
 from flask import Flask, render_template
 from models import storage
@@ -12,17 +13,21 @@ app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close(cmd):
-    """Closes a session"""
+def close_db(exc):
+    """close the current session of sqlalchemist"""
     storage.close()
 
 
-@app.route("/states_list")
-def state_list():
-    """lists states in the database"""
+@app.route('/states_list')
+def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)

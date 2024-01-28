ff31c6084e309b3ee862b1a7a16744ef073804cc
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-28 23:31:37 +0300

    .

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -40,5 +40,5 @@ def cities_by_states(id=None):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
-#    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
